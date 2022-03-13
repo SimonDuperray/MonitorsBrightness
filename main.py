a1e91fb52fb2b4566c00c0f5558ba653f403e6da
Simon <simon.duperray@reseau.eseo.fr>
2022-03-13 21:04:38 +0100

    :sparkles: create functions

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("init")
+import screen_brightness_control as sbc
+
+def set_middle():
+   for i in range(3):
+      sbc.set_brightness(value=50, display=i)
+
+
+def set_full():
+   for i in range(3):
+      sbc.set_brightness(value=100, display=i)
+
+
+set_middle()
+# set_full()
